c61187a340b7326a65515282be1af18628dfabfb
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-13 03:25:51 +0000

    Updated stats

--- Items/EnchantedPickaxe.cs
+++ Items/EnchantedPickaxe.cs
@@ -5,7 +5,7 @@ namespace EnchantedToolsMod.Items {
 	public class EnchantedPickaxe : ModItem {
 		public override void SetStaticDefaults() {
 			DisplayName.SetDefault("Enchanted Pickaxe");
-			Tooltip.SetDefault("Shoots an enchanted beam");
+			// Tooltip.SetDefault("Shoots an enchanted beam");
 		}
 		public override void SetDefaults() {
 			item.damage = 10;
@@ -16,10 +16,12 @@ namespace EnchantedToolsMod.Items {
 			item.useAnimation = 20;
 			item.useStyle = 1;
 			item.knockBack = 4;
-			item.value = 10000;
+			item.value = 50000;
 			item.rare = 1;
 			item.UseSound = SoundID.Item1;
 			item.autoReuse = true;
+			item.melee = true;
+			item.pick = 65;
 		}
 	}
 }
