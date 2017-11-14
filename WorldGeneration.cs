c46af4b602dea01d8113d0abb2b16fdaa3602f38
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-14 08:06:41 +0000

    Added code to generate in chests

--- WorldGeneration.cs
+++ WorldGeneration.cs
@@ -1,9 +1,29 @@
-﻿using Terraria.ModLoader;
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace EnchantedTools {
     public class WorldGeneration : ModWorld {
         public override void PostWorldGen() {
-            
+            var items = new int[] {
+                mod.ItemType("EnchantedPickaxe")
+            };
+
+            var itemChoice = 0;
+
+            for (var chestIndex = 0; chestIndex < 1000; chestIndex++) {
+                var chest = Main.chest[chestIndex];
+
+                if (chest != null && Main.tile[chest.x, chest.y].type == TileID.Containers && Main.tile[chest.x, chest.y].frameX == 11 * 36) {
+                    for (int inventoryIndex = 0; inventoryIndex < 40; inventoryIndex++) {
+                        if (chest.item[inventoryIndex].type == 1) {
+                            chest.item[inventoryIndex].SetDefaults(items[itemChoice]);
+                            itemChoice = (itemChoice + 1) % items.Length;
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
