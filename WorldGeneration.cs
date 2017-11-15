0283df4df9a0021b98cf9d8a91265ffa353672a0
DeflatedPickle <DeflatedPickle@gmail.com>
2017-11-15 07:31:21 +0000

    Cleaned up code

--- WorldGeneration.cs
+++ WorldGeneration.cs
@@ -5,7 +5,7 @@ using Terraria.ModLoader;
 namespace EnchantedTools {
     public class WorldGeneration : ModWorld {
         public override void PostWorldGen() {
-            var items = new int[] {
+            var items = new[] {
                 mod.ItemType("EnchantedPickaxe"),
                 mod.ItemType("EnchantedAxe"),
                 mod.ItemType("EnchantedHammer")
@@ -13,17 +13,15 @@ namespace EnchantedTools {
 
             var itemChoice = 0;
 
-            for (int chestIndex = 0; chestIndex < 1000; chestIndex++) {
-                Chest chest = Main.chest[chestIndex];
+            for (var chestIndex = 0; chestIndex < 1000; chestIndex++) {
+                var chest = Main.chest[chestIndex];
 
-                if (chest != null && Main.tile[chest.x, chest.y].type == TileID.Containers && Main.tile[chest.x, chest.y].frameX == 1 * 36) {
-                    for (int inventoryIndex = 0; inventoryIndex < 40; inventoryIndex++) {
-                        if (chest.item[inventoryIndex].type == 0) {
-                            chest.item[inventoryIndex].SetDefaults(items[itemChoice]);
-                            itemChoice = (itemChoice + 1) % items.Length;
-                            break;
-                        }
-                    }
+                if (chest == null || Main.tile[chest.x, chest.y].type != TileID.Containers || Main.tile[chest.x, chest.y].frameX != 1 * 36) continue;
+                for (var inventoryIndex = 0; inventoryIndex < 40; inventoryIndex++) {
+                    if (chest.item[inventoryIndex].type != 0) continue;
+                    chest.item[inventoryIndex].SetDefaults(items[itemChoice]);
+                    itemChoice = (itemChoice + 1) % items.Length;
+                    break;
                 }
             }
         }
